cadc99a69e29b2b9cd3bd0b1bbb309af9318283e
Anderson Laventure <Laventurepsu1@gmail.com>
2026-05-05 14:48:30 -0400

    Add Todo class with properties and methods for task management

---
+++ todo-api/src/main/java/com/anderson/todoapi/Todo.java
@@ -0,0 +1,22 @@
+package com.anderson.todoapi;
+
+public class Todo {
+    private long id;
+    private String text;
+    private boolean done;
+
+    public Todo() {}
+
+    public Todo(long id, String text, boolean done) {
+        this.id = id;
+        this.text = text;
+        this.done = done;
+    }
+
+    public long getId() { return id; }
+    public void setId(long id) { this.id = id; }
+    public String getText() { return text; }
+    public void setText(String text) { this.text = text; }
+    public boolean isDone() { return done; }
+    public void setDone(boolean done) { this.done = done; }
+}
